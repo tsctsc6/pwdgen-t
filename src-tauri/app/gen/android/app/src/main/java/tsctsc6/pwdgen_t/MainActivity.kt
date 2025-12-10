25e1351fc63742bcae18dce4f993ae43600bc91a
tsctsc6 <tsctsc6@126.com>
2025-12-10 15:11:03 +0800

    first commit

---
+++ src-tauri/app/gen/android/app/src/main/java/tsctsc6/pwdgen_t/MainActivity.kt
@@ -0,0 +1,11 @@
+package tsctsc6.pwdgen_t
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
