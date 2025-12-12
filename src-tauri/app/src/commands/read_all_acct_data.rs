4b1bac5b8235c6db4cb7889779b41b9457a1b7d5
tsctsc6 <tsctsc6@126.com>
2025-12-12 18:18:10 +0800

    change the return type of read_all_acct_data

--- src-tauri/app/src/commands/read_all_acct_data.rs
+++ src-tauri/app/src/commands/read_all_acct_data.rs
@@ -1,9 +1,11 @@
 ﻿use crate::commands::CommandError;
 use crate::entities::acct_data;
-use crate::entities::acct_data::Model;
 use crate::factory::create_db_connection;
 use migration::Condition;
-use sea_orm::{ColumnTrait, EntityTrait, Order, PaginatorTrait, QueryFilter, QueryOrder};
+use sea_orm::{
+    ColumnTrait, DerivePartialModel, EntityTrait, Order, PaginatorTrait, QueryFilter, QueryOrder,
+};
+use serde::Serialize;
 
 #[tauri::command]
 pub async fn read_all_acct_data(
@@ -11,7 +13,7 @@ pub async fn read_all_acct_data(
     search_term: String,
     page_index: u64,
     page_size: u64,
-) -> Result<Vec<Model>, CommandError> {
+) -> Result<Vec<ReadAllAcctDataResult>, CommandError> {
     let db = create_db_connection(&app).await?;
     let acct_data_to_read_query = acct_data::Entity::find()
         .filter(
@@ -20,7 +22,16 @@ pub async fn read_all_acct_data(
                 .add(acct_data::Column::UserName.contains(&search_term)),
         )
         .order_by(acct_data::Column::Id, Order::Asc)
+        .into_partial_model()
         .paginate(&db, page_size);
     let acct_data_to_read = acct_data_to_read_query.fetch_page(page_index).await?;
     Ok(acct_data_to_read)
+}
+
+#[derive(DerivePartialModel, Serialize)]
+#[sea_orm(entity = "acct_data::Entity")]
+pub struct ReadAllAcctDataResult {
+    pub id: i32,
+    pub user_name: String,
+    pub platform: String,
 }
