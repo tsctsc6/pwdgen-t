1c6e93dafa447fe664938eec08a6f8212c3827f5
tsctsc6 <tsctsc6@126.com>
2025-12-15 19:21:23 +0800

    impl add acct data

--- src/pages/AddAcctData.svelte
+++ src/pages/AddAcctData.svelte
@@ -1,7 +1,8 @@
 ﻿<script lang="ts">
-    import {Button, Label, Input, Toggle,ButtonGroup,Clipboard,Tooltip, } from "flowbite-svelte";
+    import {Button, Label, Input, Toggle, ButtonGroup, Clipboard, Tooltip,} from "flowbite-svelte";
     import {FloppyDiskAltSolid, MinusOutline, PlusOutline, CheckOutline, ClipboardCleanSolid, PlaySolid}
         from "flowbite-svelte-icons";
+    import {invoke} from "@tauri-apps/api/core";
 
     let userName: string = $state("");
     let platform: string = $state("");
@@ -12,11 +13,22 @@
     let useNumber: boolean = $state(true);
     let useSpecialCharacter: boolean = $state(true);
     let passwordLength: number = $state(15);
-    let mainPassword : string = $state("");
+    let mainPassword: string = $state("");
     let passwordGenerated: string = $state("");
 
-    const onSave = () => {
-        console.log("onSave");
+    const onSave = async () => {
+        let request = {
+            user_name: userName,
+            platform: platform,
+            remark: remark,
+            skip_count: skipCount,
+            use_up_letter: useUpLetter,
+            use_low_letter: useLowLetter,
+            use_number: useNumber,
+            use_sp_char: useSpecialCharacter,
+            pwd_len: passwordLength,
+        };
+        await invoke("create_acct_data", {request});
     }
 
     const onGenerate = () => {
@@ -30,33 +42,33 @@
     </h1>
 
     <Button id="submit" pill class="p-2!" onclick={onSave}>
-        <FloppyDiskAltSolid class="h-6 w-6" />
+        <FloppyDiskAltSolid class="h-6 w-6"/>
     </Button>
 </div>
 
 <div class="flex items-center mb-6 gap-2">
     <Label for="user-name" class="mb-2 block">User name</Label>
-    <Input id="user-name" placeholder="User name" bind:value={userName} />
+    <Input id="user-name" placeholder="User name" bind:value={userName}/>
 </div>
 <div class="flex items-center mb-6 gap-2">
     <Label for="platform" class="mb-2 block">Platform</Label>
-    <Input id="platform" placeholder="Platform" bind:value={platform} />
+    <Input id="platform" placeholder="Platform" bind:value={platform}/>
 </div>
 <div class="flex items-center mb-6 gap-2">
     <Label for="remark" class="mb-2 block">Remark</Label>
-    <Input id="remark" placeholder="Remark" bind:value={remark} />
+    <Input id="remark" placeholder="Remark" bind:value={remark}/>
 </div>
 <div class="flex items-center mb-6 gap-2">
     <Label for="skip-count" class="mb-2 block">Skip count</Label>
     <div class="relative flex max-w-56 items-center">
         <ButtonGroup>
             <Button type="button" id="decrement-button" onclick={() => (skipCount -= 1)}>
-                <MinusOutline />
+                <MinusOutline/>
             </Button>
             <Input bind:value={skipCount} type="number" id="skip-count-input"
-                   aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center" />
+                   aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center"/>
             <Button type="button" id="increment-button" onclick={() => (skipCount += 1)}>
-                <PlusOutline />
+                <PlusOutline/>
             </Button>
         </ButtonGroup>
     </div>
@@ -78,12 +90,12 @@
     <div class="relative flex max-w-56 items-center">
         <ButtonGroup>
             <Button type="button" id="decrement-button" onclick={() => (passwordLength -= 1)}>
-                <MinusOutline />
+                <MinusOutline/>
             </Button>
             <Input bind:value={passwordLength} type="number" id="password-length-input"
-                   aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center" />
+                   aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center"/>
             <Button type="button" id="increment-button" onclick={() => (passwordLength += 1)}>
-                <PlusOutline />
+                <PlusOutline/>
             </Button>
         </ButtonGroup>
     </div>
@@ -92,9 +104,9 @@
 <div class="flex items-center mb-6 gap-2"></div>
 
 <div class="flex items-center mb-6 gap-2">
-    <Input id="default-input" type="password" bind:value={mainPassword} />
+    <Input id="default-input" type="password" bind:value={mainPassword}/>
     <Button id="generate" pill class="p-2!" onclick={onGenerate}>
-        <PlaySolid class="h-6 w-6" />
+        <PlaySolid class="h-6 w-6"/>
     </Button>
 </div>
 <div class="flex items-center mb-6 gap-2">
@@ -102,7 +114,11 @@
     <Clipboard color="primary" bind:value={passwordGenerated}>
         {#snippet children(success)}
             <Tooltip class="whitespace-nowrap">{success ? "Copied" : "Copy to clipboard"}</Tooltip>
-            {#if success}<CheckOutline />{:else}<ClipboardCleanSolid />{/if}
+            {#if success}
+                <CheckOutline/>
+            {:else}
+                <ClipboardCleanSolid/>
+            {/if}
         {/snippet}
     </Clipboard>
 </div>
