b71a17cf344fb3d1bb61f27906932517c4b88e4c
tsctsc6 <tsctsc6@126.com>
2025-12-15 17:19:10 +0800

    change the return type of read_all_acct_data

--- src-tauri/app/src/commands/read_all_acct_data.rs
+++ src-tauri/app/src/commands/read_all_acct_data.rs
@@ -13,9 +13,9 @@ pub async fn read_all_acct_data(
     search_term: String,
     page_index: u64,
     page_size: u64,
-) -> Result<Vec<ReadAllAcctDataResult>, CommandError> {
+) -> Result<ReadAllAcctDataResult, CommandError> {
     let db = create_db_connection(&app).await?;
-    let acct_data_to_read_query = acct_data::Entity::find()
+    let acct_data_query = acct_data::Entity::find()
         .filter(
             Condition::any()
                 .add(acct_data::Column::Platform.contains(&search_term))
@@ -24,14 +24,24 @@ pub async fn read_all_acct_data(
         .order_by(acct_data::Column::Id, Order::Asc)
         .into_partial_model()
         .paginate(&db, page_size);
-    let acct_data_to_read = acct_data_to_read_query.fetch_page(page_index).await?;
-    Ok(acct_data_to_read)
+    let acct_data_list = acct_data_query.fetch_page(page_index).await?;
+    let result = ReadAllAcctDataResult {
+        page_count: acct_data_query.num_pages().await?,
+        page_content: acct_data_list,
+    };
+    Ok(result)
 }
 
 #[derive(DerivePartialModel, Serialize)]
 #[sea_orm(entity = "acct_data::Entity")]
-pub struct ReadAllAcctDataResult {
+pub struct AcctDataPartialModel {
     pub id: i32,
     pub user_name: String,
     pub platform: String,
+}
+
+#[derive(Serialize)]
+pub struct ReadAllAcctDataResult {
+    pub page_count: u64,
+    pub page_content: Vec<AcctDataPartialModel>,
 }
