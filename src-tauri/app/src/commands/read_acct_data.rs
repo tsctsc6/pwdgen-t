5997d303ccc06281e85d36000346c365bb67bee8
tsctsc6 <tsctsc6@126.com>
2025-12-16 19:17:18 +0800

    change return type of read_acct_data

--- src-tauri/app/src/commands/read_acct_data.rs
+++ src-tauri/app/src/commands/read_acct_data.rs
@@ -1,13 +1,19 @@
 ﻿use crate::commands::{CommandError, UniversalError};
 use crate::entities::acct_data;
-use crate::entities::acct_data::Model;
 use crate::factory::create_db_connection;
-use sea_orm::EntityTrait;
+use sea_orm::{DerivePartialModel, EntityTrait};
+use serde::Serialize;
 
 #[tauri::command]
-pub async fn read_acct_data(app: tauri::AppHandle, id: i32) -> Result<Model, CommandError> {
+pub async fn read_acct_data(
+    app: tauri::AppHandle,
+    id: i32,
+) -> Result<ReadAcctDataResult, CommandError> {
     let db = create_db_connection(&app).await?;
-    let acct_data_to_read = acct_data::Entity::find_by_id(id).one(&db).await?;
+    let acct_data_to_read = acct_data::Entity::find_by_id(id)
+        .into_partial_model()
+        .one(&db)
+        .await?;
     let acct_data_to_read = match acct_data_to_read {
         None => Err(UniversalError {
             code: 1,
@@ -16,4 +22,20 @@ pub async fn read_acct_data(app: tauri::AppHandle, id: i32) -> Result<Model, Com
         Some(x) => x,
     };
     Ok(acct_data_to_read)
+}
+
+#[derive(DerivePartialModel, Serialize)]
+#[sea_orm(entity = "acct_data::Entity")]
+pub struct ReadAcctDataResult {
+    pub id: i32,
+    pub user_name: String,
+    pub platform: String,
+    pub remark: String,
+    pub skip_count: u32,
+    pub use_up_letter: bool,
+    pub use_low_letter: bool,
+    pub use_number: bool,
+    pub use_sp_char: bool,
+    pub pwd_len: u32,
+    pub updated_at: String,
 }
--- src-tauri/app/src/entities/acct_data.rs
+++ src-tauri/app/src/entities/acct_data.rs
@@ -1,8 +1,7 @@
 ﻿use sea_orm::entity::prelude::*;
-use serde::{Deserialize, Serialize};
 
 #[sea_orm::model]
-#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
 #[sea_orm(table_name = "AcctData", rename_all = "PascalCase")]
 pub struct Model {
     #[sea_orm(primary_key)]
