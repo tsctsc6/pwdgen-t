39233756f69e5d850151b185dec74680a0e6d4fe
tsctsc6 <tsctsc6@126.com>
2025-12-16 19:39:29 +0800

    wirte up read acct data page

--- src/pages/ReadAcctData.svelte
+++ src/pages/ReadAcctData.svelte
@@ -1,6 +1,6 @@
 ﻿<script lang="ts">
-    import {Button, Label, Input, Clipboard, Tooltip, Spinner} from "flowbite-svelte";
-    import {CaretLeftSolid, CheckOutline, ClipboardCleanSolid, PlaySolid}
+    import {Button, Label, Input, Clipboard, Tooltip, Spinner, ButtonGroup, Toggle} from "flowbite-svelte";
+    import {CaretLeftSolid, CheckOutline, ClipboardCleanSolid, PlaySolid, EditSolid, MinusOutline, PlusOutline}
         from "flowbite-svelte-icons";
     import {invoke} from "@tauri-apps/api/core";
     import {getContext, onMount} from "svelte";
@@ -25,12 +25,15 @@
         router.back();
     }
 
+    const onEdit = () => {
+    }
+
     const onGenerate = () => {
         console.log("onGenerate");
     }
 </script>
 
-<div class="flex items-center mb-6 gap-2">
+<div class="flex items-center mb-6 gap-3">
     <Button id="submit" pill class="p-2!" onclick={onBack}>
         <CaretLeftSolid class="h-6 w-6"/>
     </Button>
@@ -38,46 +41,76 @@
     <h1 class="page-title flex-1">
         Details
     </h1>
+
+    <Button id="submit" pill class="p-2!" onclick={onEdit}>
+        <EditSolid class="h-6 w-6"/>
+    </Button>
 </div>
 
 {#if acctData === null}
     <Spinner/>
 {:else}
     <div class="flex items-center mb-6 gap-2">
+        <Label for="id" class="mb-2 block">Id</Label>
+        <Input readonly id="id" bind:value={acctData.id}/>
+    </div>
+    <div class="flex items-center mb-6 gap-2">
         <Label for="user-name" class="mb-2 block">User name</Label>
-        <Label id="user-name">{acctData.user_name}</Label>
+        <Input readonly id="user-name" bind:value={acctData.user_name}/>
     </div>
     <div class="flex items-center mb-6 gap-2">
         <Label for="platform" class="mb-2 block">Platform</Label>
-        <Label id="platform">{acctData.platform}</Label>
+        <Input readonly id="platform" bind:value={acctData.platform}/>
     </div>
     <div class="flex items-center mb-6 gap-2">
         <Label for="remark" class="mb-2 block">Remark</Label>
-        <Label id="remark">{acctData.remark}</Label>
+        <Input readonly id="remark" bind:value={acctData.remark}/>
     </div>
     <div class="flex items-center mb-6 gap-2">
         <Label for="skip-count" class="mb-2 block">Skip count</Label>
-        <Label id="skip-count">{acctData.skip_count}</Label>
+        <div class="relative flex max-w-56 items-center">
+            <ButtonGroup>
+                <Button disabled type="button" id="decrement-button">
+                    <MinusOutline/>
+                </Button>
+                <Input readonly bind:value={acctData.skip_count} type="number" id="skip-count-input"
+                       aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center"/>
+                <Button disabled type="button" id="increment-button">
+                    <PlusOutline/>
+                </Button>
+            </ButtonGroup>
+        </div>
     </div>
     <div class="mb-6">
-        <Label for="use-up-letter" class="mb-2 block">Use up letter</Label>
-        <Label id="use-up-letter">{acctData.use_up_letter}</Label>
+        <Toggle disabled checked={acctData.use_up_letter}>Use up letter</Toggle>
     </div>
     <div class="mb-6">
-        <Label for="use-low-letter" class="mb-2 block">Use low letter</Label>
-        <Label id="use-low-letter">{acctData.use_low_letter}</Label>
+        <Toggle disabled checked={acctData.use_low_letter}>Use low letter</Toggle>
     </div>
     <div class="mb-6">
-        <Label for="use-number" class="mb-2 block">Use number</Label>
-        <Label id="use-number">{acctData.use_number}</Label>
+        <Toggle disabled checked={acctData.use_number}>Use number</Toggle>
     </div>
     <div class="mb-6">
-        <Label for="use-special-character" class="mb-2 block">Use special character</Label>
-        <Label id="use-special-character">{acctData.use_sp_char}</Label>
+        <Toggle disabled checked={acctData.use_sp_char}>Use special character</Toggle>
     </div>
-    <div class="mb-6">
-        <Label for="use-up-letter" class="mb-2 block">Password length</Label>
-        <Label id="use-up-letter">{acctData.pwd_len}</Label>
+    <div class="flex items-center mb-6 gap-2">
+        <Label for="password-length" class="mb-2 block">Password length</Label>
+        <div class="relative flex max-w-56 items-center">
+            <ButtonGroup>
+                <Button disabled type="button" id="decrement-button">
+                    <MinusOutline/>
+                </Button>
+                <Input readonly bind:value={acctData.pwd_len} type="number" id="password-length-input"
+                       aria-describedby="helper-text-explanation" placeholder="999" required class="w-32! text-center"/>
+                <Button disabled type="button" id="increment-button">
+                    <PlusOutline/>
+                </Button>
+            </ButtonGroup>
+        </div>
+    </div>
+    <div class="flex items-center mb-6 gap-2">
+        <Label for="updated-at" class="mb-2 block">Updated at</Label>
+        <Input readonly id="updated-at" bind:value={acctData.updated_at}/>
     </div>
 
     <div class="flex items-center mb-6 gap-2"></div>
