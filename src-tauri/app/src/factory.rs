dd529f38e82807a20825fb3c43001ccb8a4f90a2
tsctsc6 <tsctsc6@126.com>
2025-12-18 12:01:27 +0800

    if is dev environment, create database at "."

--- src-tauri/app/src/factory.rs
+++ src-tauri/app/src/factory.rs
@@ -6,8 +6,13 @@ use tokio::fs;
 pub async fn create_db_connection(
     app: &tauri::AppHandle,
 ) -> Result<DatabaseConnection, CommandError> {
-    let app_data_path = app.path().app_data_dir()?.to_string_lossy().to_string();
-    // println!("{}", app_data_path);
+    let app_data_path: String;
+    if tauri::is_dev() {
+        app_data_path = ".".to_string();
+    } else {
+        app_data_path = app.path().app_data_dir()?.to_string_lossy().to_string();
+    }
+    dbg!(&app_data_path);
 
     fs::create_dir_all(&app_data_path).await?;
 
